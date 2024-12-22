3a91ee342ab7cc0f2c4195e4e6509de5212638d8
Asala El Ghandour <asala.elghandour@gmail.com>
2024-12-22 22:50:58 +0200

    using dapr for services communications betwen the data service and suppliers service

--- services/suppliers-service/src/Services/SupplierService.ts
+++ services/suppliers-service/src/Services/SupplierService.ts
@@ -1,16 +1,28 @@
 // src/services/SupplierService.ts
 import axios from "axios";
 import { Supplier } from "../types";
+import { DaprClient, HttpMethod } from "@dapr/dapr";
 
 const SUPPLIERS_DATA_SERVICE_URL = process.env.SUPPLIERS_DATA_SERVICE_URL;
+const daprHost = process.env.DAPR_HOST;
+const daprPort = process.env.DAPR_PORT;
+const client = new DaprClient({
+  daprHost,
+  daprPort,
+});
+
+const targetAppId = process.env.DAPR_TARGET_APP || "";
+
 export class SupplierService {
   static async createSupplier(data: Partial<Supplier>) {
     try {
-      const response = await axios.post(
-        `${SUPPLIERS_DATA_SERVICE_URL}/suppliers`,
+      const response = await client.invoker.invoke(
+        targetAppId,
+        "suppliers",
+        HttpMethod.POST,
         data
       );
-      return response.data;
+      return response;
     } catch (error) {
       console.error("Error updating supplier:", error);
       const errorMessage =
@@ -21,11 +33,13 @@ export class SupplierService {
 
   static async updateSupplier(id: string, data: Partial<Supplier>) {
     try {
-      const response = await axios.put(
-        `${SUPPLIERS_DATA_SERVICE_URL}/suppliers/${id}`,
+      const response = await client.invoker.invoke(
+        targetAppId,
+        `suppliers/${id}`,
+        HttpMethod.PUT,
         data
       );
-      return response.data;
+      return response;
     } catch (error) {
       console.error("Error updating supplier:", error);
       const errorMessage =
@@ -36,10 +50,12 @@ export class SupplierService {
 
   static async deleteSupplier(id: string) {
     try {
-      const response = await axios.delete(
-        `${SUPPLIERS_DATA_SERVICE_URL}/suppliers/${id}`
+      const response = await client.invoker.invoke(
+        targetAppId,
+        `suppliers/${id}`,
+        HttpMethod.DELETE
       );
-      return response.data;
+      return response;
     } catch (error) {
       console.error("Error deleting supplier:", error);
       const errorMessage =
@@ -48,12 +64,18 @@ export class SupplierService {
     }
   }
 
-  static async getSuppliers() {
+  static async getSuppliers(
+    filters: Partial<Supplier>,
+    sort: { [key: string]: "ASC" | "DESC" }
+  ) {
     try {
-      const response = await axios.get(
-        `${SUPPLIERS_DATA_SERVICE_URL}/suppliers`
+      const response = await client.invoker.invoke(
+        targetAppId,
+        `suppliers`,
+        HttpMethod.GET,
+        { filters, sort }
       );
-      return response.data;
+      return response;
     } catch (error) {
       console.error("Error fetching suppliers:", error);
       const errorMessage =
