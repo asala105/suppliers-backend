8107d6849ba5fab36b9c32e3ff0b00ef9689c06d
Asala El Ghandour <asala.elghandour@gmail.com>
2024-12-24 03:36:28 +0200

    fix migration

--- services/suppliers-data-service/src/migrations/1734817525556-CreateSupplierTable.ts
+++ services/suppliers-data-service/src/migrations/1734817525556-CreateSupplierTable.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class CreateSupplierTable1734817525556 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-          CREATE TABLE "supplier" (
+          CREATE TABLE "suppliers" (
             "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
             "name" character varying NOT NULL,
             "vatNumber" character varying NOT NULL,
